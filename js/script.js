2a572d332eb5e2e7b579ee9f6013c3e3e304feb4
volk22kamin <69310378+volk22kamin@users.noreply.github.com>
2021-12-13 13:14:26 +0200

    finished project

--- js/script.js
+++ js/script.js
@@ -3,32 +3,44 @@ const quoteText = document.getElementById('quote');
 const authorName = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('newQuote');
+const loader = document.getElementById('loader');
 
 
 
 
 let apiQuotes = [];
 
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner(){
+    quoteContainer.hidden = false; 
+    loader.hidden = true;
+}
+
 
 // new quote
 function newQuote(){
+    showLoadingSpinner();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // check if author nae is blank and replace with 'unknown'
     if(!quote.author){
         authorName.textContent = 'Unknown';
     } else {
         authorName.textContent = quote.author;
     }
-    // if the quote is too long
     if(quote.text.length > 120){
         quoteText.classList.add('longQuote');
     } else{
         quoteText.classList.remove('longQuote');
     }
     quoteText.textContent = quote.text;
+    removeLoadingSpinner();
 }
-// get quotes from API
-async function getQuotes(){
+
+async function getQuotesFromAPI(){
+    showLoadingSpinner();
     const apiURL = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiURL);
@@ -53,4 +65,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 
 // on load
-getQuotes();
+getQuotesFromAPI();
